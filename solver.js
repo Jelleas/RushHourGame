d5d073873ba7c7af8839fc2cb800e467323ff39c
Jelle van Assema <jelle.van.assema@gmail.com>
2015-07-06 13:38:10 +0200

    Start on board

--- solver.js
+++ solver.js
@@ -38,14 +38,32 @@ function Line(linecode) {
 // TODO
 function generateLines() {
 	function isValidLine(line) {
+		var length = 0;
+		var previousElem = 0;
 		for (var i = 0; i < line.size; i++) {
-			var codeElem = line
+			var codeElem = line[i];
+
+			if (codeElem) {
+				length += 1;
+			}
+
 			previousElem = codeElem;
 		}
 	}
 }
 
+function Board(lines) {
+	this.lines = lines;
+}
+
+
+
+// Test code
+var board = new Board([Line(true,true,false,true,false,false)]);
+
 var line = new Line([true, false, true, false, false, false]);
 
 console.log(line.code);
 console.log(line.occupation);
+
+
